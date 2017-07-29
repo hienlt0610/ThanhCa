592e5c6efe7b578811839be15106f5422d9638be
Hien Le <hienlt0610@gmail.com>
2017-07-29 22:29:11 +0700

    + Init commit

---
+++ app/src/main/java/dev/hienlt0610/thanhca/MainActivity.java
@@ -0,0 +1,13 @@
+package dev.hienlt0610.thanhca;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
